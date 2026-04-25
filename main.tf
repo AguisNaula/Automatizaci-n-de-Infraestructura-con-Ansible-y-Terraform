c73da50400589e43b379d45e368116c2ca9e3064
AguisNaula <131500825+AguisNaula@users.noreply.github.com>
2026-04-25 17:32:28 -0400

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,19 @@
+terraform {
+ required_providers {
+ docker = {
+ source = "kreuzwerker/docker"
+ }
+ }
+}
+provider "docker" {}
+resource "docker_image" "nginx" {
+ name = "nginx:latest"
+}
+resource "docker_container" "web" {
+ name = "mi-servidor-web"
+ image = docker_image.nginx.name
+ ports {
+ internal = 80
+ external = 8080
+ }
+}
